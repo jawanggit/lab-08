fc5b9861dfec13d01fe9e49bec0e1bf52893fc42
James Wang <jawang.um@gmail.com>
2020-06-18 21:25:13 -0700

    completed feature4

--- server.js
+++ server.js
@@ -48,56 +48,11 @@ function Weather(obj){
   this.time = obj.datetime  
 }
 
+app.use((error, request, answer, next) => {
+  console.log(error);
+  answer.status(500).send('Sorry, something went wrong');
+});
 
-
-// // $('thing').on('something', () => {})
-// app.get('/restaurants', (request, response) => {
-//   let data = require('./data/restaurants.json');
-
-//   let allRestaurants = [];
-//   data.nearby_restaurants.forEach( restObject => {
-//     let restaurant = new Restaurant(restObject);
-//     allRestaurants.push(restaurant);
-//   });
-
-//   response.status(200).json(allRestaurants);
-// });
-
-// function Restaurant(obj) {
-//   this.restaurant = obj.restaurant.name;
-//   this.locality = obj.restaurant.location.locality;
-//   this.cuisines = obj.restaurant.cuisines;
-// }
-
-// app.put(), app.delete(), app.post()
-
-// app.use('*', (request,response) => {
-//   response.status(404).send('Huh?');
-// });
-
-// app.use((error, request, response, next) => {
-//   console.log(error);
-//   response.status(500).send('server is broken');
-// });
 
 app.listen( PORT, () => console.log('Server Port: ', PORT));
 
-// Handle a request for location data
-// Get a city from the client
-// Fetch data from an API
-// Adapt the data, using a Constructor Function
-// Send the adapted data to the client
-
-
-// Locaton Constructor Function
-// Take in some big object, turn it into something that matches the contract
-
-
-// Handle a request for restaurant data
-// Get location information from the client (lat,long,city-name)
-// Fetch data from an API
-// Adapt the data, using a Constructor Function
-// Send the adapted data to the client
-
-// Restaurant Constructor Function
-// Take in some big object, turn it into something that matches the contract
